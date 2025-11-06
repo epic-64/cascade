4e336299b28ba2cbdfa3b4500dd029a58742a0fc
epic-64 <william.raendchen@gmail.com>
2025-11-07 00:00:00 +0100

    update ClientMain to use new entrypoint method and demonstrate shared types

--- js/src/main/scala/cascade/client/ClientMain.scala
+++ js/src/main/scala/cascade/client/ClientMain.scala
@@ -2,15 +2,10 @@ package cascade.client
 
 import cascade.shared.{User, SharedGreeter}
 
-object ClientMain:
-  def main(args: Array[String]): Unit =
-    println("Hello, world!")
-
-
-//@main def clientMain(): Unit =
-//  // Simple Scala.js entrypoint demonstrating use of shared types
-//  val sample = User(42, "Alice")
-//  val msg    = SharedGreeter.greet(sample)
-//  // For now we just log to the JS console; no DOM dependency required
-//  println(s"[client] $msg")
+@main def clientMain(): Unit =
+  // Simple Scala.js entrypoint demonstrating use of shared types
+  val sample = User(42, "Alice")
+  val msg    = SharedGreeter.greet(sample)
+  // For now we just log to the JS console; no DOM dependency required
+  println(s"[client] $msg")
 
