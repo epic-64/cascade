4e3d1dfc32f2e865427e7497bd33352e9e8c0f6c
epic-64 <william.raendchen@gmail.com>
2026-02-11 00:00:00 +0100

    add cache-busting for static assets and implement version endpoint

--- jvm/src/main/scala/server/WebServer.scala
+++ jvm/src/main/scala/server/WebServer.scala
@@ -28,6 +28,10 @@ object WebServer extends MainRoutes:
   def health(): ujson.Value =
     HealthEndpoint.health(startTime)
 
+  @cask.get("/api/version")
+  def version(): ujson.Value =
+    VersionEndpoint.version(startTime)
+
   @cask.get("/api/weather/:city")
   def weather(city: String): ujson.Value =
     WeatherEndpoint.getWeather(city)
