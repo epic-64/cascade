24bf26cff68d1fe99a5b063b9ad4a652b49b34be
epic-64 <william.raendchen@gmail.com>
2026-03-02 00:00:00 +0100

    update bureau auto-upgrade logic to perform multiple upgrades based on elapsed time

--- js/src/main/scala/client/components/laminar/tilekingdom/tiles/LockedTile.scala
+++ js/src/main/scala/client/components/laminar/tilekingdom/tiles/LockedTile.scala
@@ -3,17 +3,26 @@ package client.components.laminar.tilekingdom.tiles
 import com.raquo.laminar.api.L.*
 import shared.TileKingdom.*
 import client.components.laminar.tilekingdom.{TileGridState, TileUtils}
+import client.components.laminar.TileKingdomState
 
 /** Locked tile component.
   *
-  * Displays as a dark gray tile with no content, showing island bounds.
+  * Displays as a dark gray tile showing island bounds.
+  * Shows the gold cost required to unlock when player can't afford it.
   */
 object LockedTile:
 
   def apply(coord: Coord): HtmlElement =
+    val costSignal = TileKingdomState.nextTileUnlockCostSignal
+    
     div(
       idAttr := TileUtils.tileId(coord),
       cls := "tile-kingdom-tile locked-empty",
-      styleAttr <-- TileGridState.tileStyle(coord)
+      styleAttr <-- TileGridState.tileStyle(coord),
+      
+      div(
+        cls := "locked-cost",
+        child.text <-- costSignal.map(cost => TileUtils.formatNumber(cost))
+      )
     )
 
